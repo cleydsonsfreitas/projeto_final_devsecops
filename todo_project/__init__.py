b2c7b8ad7f48e38f06173b92b4734765a75ae853
Cleydson Freitas <cleydson@exemplo.com>
2026-06-14 19:26:23 -0300

    fix(security): aplicar tag nosec correta para mitigar falha do bandit

--- todo_project/__init__.py
+++ todo_project/__init__.py
@@ -3,14 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
+app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'  # nosec
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
-login_manager.login_view = 'login' 
+login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
 
 bcrypt = Bcrypt(app)
